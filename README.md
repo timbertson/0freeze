a00da08befa44e2bef1d3267560751e343e672d9
Tim Cuthbertson <tim@gfxmonk.net>
2013-08-14 18:54:55 +1000

    basic implementation

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # 0freeze
 
-Creates a version-frozen version of a zeroinstall feed.
+Creates a version-frozen copy of a zeroinstall feed (including dependencies).
 
-This is similar but more portable to a selections document. A selections document
+The result is similar to but more portable than a selections document. A selections document
 (obtained from `0install select --xml INTERFACE`) is locked to specific implementation IDs,
 package names and local feed paths.
 
+In contrast, 0freeze only locks down versions, so the generated feed can be used
+cross-platform - as long as the given version is available on all required platforms.
+
+Additionally, the level of freezing can be controlled with the --components option.
+
+Package implementations are not version-restrcicted beyond what is already
+specified in the original feed.
+
+Local feeds are not included as dependencies (since they include system-specific path information),
+but the version of the local implementation is still used. It's up to you to ensure that the
+same version is available wherever you run the locked feed.
+
+# Known Issues:
+
+If a published feed is modified after the locked feed is generated, it's possible to introduce
+new (unrestricted) dependencies. This is considered bad practice, but there is currently no
+protection against it.
--- zeroinstall_freeze.py
+++ zeroinstall_freeze.py
@@ -4,9 +4,11 @@ import optparse
 import subprocess
 import os
 import sys
+import re
 import logging
 import urllib
-import version
+import itertools
+from version import Version, VersionComponent
 from xdg import BaseDirectory
 from xml.dom import minidom
 from xml import dom
@@ -34,17 +36,32 @@ def get_local_feed_file(url):
 		logging.debug("cached feed: %s" % (cached,))
 		return cached
 
-def clean_feed_element(elem):
-	for e in elem.childNodes[:]:
+def clean_unused_commands(feed, command):
+	all_commands = feed.getElementsByTagName("command")
+	should_keep = lambda x: x.getAttribute("name") == command
+	keep = list(filter(should_keep, all_commands))
+
+	for cmd in all_commands[:]:
+		if cmd not in keep:
+			logger.debug("removing command name=%s" % (cmd.getAttribute("name")))
+			cmd.parentNode.removeChild(cmd)
+	
+	for cmd in keep:
+		cmd.setAttribute("name", "run")
+
+def clean_feed(feed, command):
+	for e in feed.childNodes[:]:
 		if e.nodeType != dom.Node.ELEMENT_NODE: continue
 		#TODO: check exhaustiveness
-		if e.tagName in ['group', 'requires', 'command', 'interface']: continue
+		if e.tagName in ['name', 'summary', 'group', 'requires', 'command', 'interface']: continue
 		logging.debug("Cleanup: remove %s" % (e.tagName))
-		elem.removeChild(e)
+		feed.removeChild(e)
+	
+	clean_unused_commands(feed, command)
 
 def isolate_implementation(feed, impl_id):
 	# get active implementation, and delete all others:
-	all_impls = feed.documentElement.getElementsByTagName("implementation")
+	all_impls = feed.getElementsByTagName("implementation")
 	is_selected_impl = lambda x: x.getAttribute("id") == impl_id
 	active_impl = filter_one(is_selected_impl, all_impls, "impl_id=%s" % impl_id)
 	logging.debug("found feed impl with id: %s" % (impl_id,))
@@ -65,13 +82,25 @@ def main():
 	p.add_option('-r', '--refresh', action='store_true')
 	p.add_option('-v', '--verbose', action='store_true')
 	p.add_option('-o', '--offline', action='store_true')
-	p.add_option('-c', '--command')
+	p.add_option('--components', type='int', help="lock down NUM version components. If NUM is positive, it locks down NUM leading components. "
+			"If NUM is negative, it locks down all but NUM trailing components. The default value is -1.\n"
+			"EXAMPLES:\n"
+			"    --components=2 will lock down foo@1.2.3 to >=1.2.3<1.3.0.\n"
+			"    --components=-2 will lock down foo@1.2.3 to >=1.2.3<1.3.0.\n"
+			"This option is ignored if --exact is used", default=-1)
+	p.add_option('--exact', action='store_true', help='use exact version (takes precedence over --components)')
+	p.add_option('--ignore', action='append', dest='ignore', default=[], metavar='URL', help="Don't restrict interface URL")
+	p.add_option('--allow-local', action='append', dest='nonlocal', default=[], metavar='URL', help="Allow URL to be satisfied by a local feed")
+	p.add_option('-c', '--command', default='run')
 	opts, args = p.parse_args()
 	logger.setLevel(level=logging.DEBUG if opts.verbose else logging.INFO)
 	input_file = args.pop(0)
+	output_file = None
 	if len(args) > 0:
 		assert len(args) == 1, 'too many arguments'
 		output_file, = args
+
+	assert opts.components != 0, "components must not be 0"
 	
 	cmd = ['0install', 'select', '--console', '--xml', input_file]
 	def add_flag(arg):
@@ -86,10 +115,9 @@ def main():
 	logging.debug("calling: %r" % (cmd,))
 	
 	selections_string = subprocess.check_output(cmd)
-	logging.debug("Got selections:\n%s" % (selections_string,))
 
 	selections = minidom.parseString(selections_string)
-	logging.debug(selections.toprettyxml())
+	logging.debug("Got selections:\n%s" % (selections.toprettyxml(),))
 
 	# URI-based feed:
 	root_iface = selections.documentElement.getAttribute("interface")
@@ -102,36 +130,79 @@ def main():
 	local_feed_path = get_local_feed_file(root_feed)
 	with open(local_feed_path) as feed_file:
 		feed = minidom.parse(feed_file)
-	logging.debug("feed contents: %s" % (feed.toprettyxml()))
+	# logging.debug("feed contents: %s" % (feed.toprettyxml()))
 
 	selected_impl_id = impl_selection.getAttribute("id")
 	if local_feed_path == root_feed and os.path.isabs(selected_impl_id):
 		selected_impl_id = os.path.relpath(selected_impl_id, os.path.dirname(local_feed_path))
 
-	active_impl = isolate_implementation(feed, selected_impl_id)
+	active_impl = isolate_implementation(feed.documentElement, selected_impl_id)
 
-	clean_feed_element(feed.documentElement)
+	clean_feed(feed.documentElement, command=opts.command)
 
 	for selection in selections.getElementsByTagName('selection'):
-		url = selection.getAttribute("from-feed") or selection.getAttribute("interface")
-		if url.startswith("distribution:"):
-			logging.info("Skipping distribution package %s" % (selection.getAttribute("package"),))
+		url = selection.getAttribute("interface")
+
+		package = selection.getAttribute("package")
+		if package:
+			logging.info("Skipping distribution package %s" % (package,))
 			continue
+		
+		if url in opts.ignore:
+			logging.info("Skipping ignored URL: %s" % (url,))
+
+		local_feed = selection.getAttribute("from-feed")
+		if local_feed and not local_feed:
+			msg = "Local feed %s used for %s" % (local_feed, url)
+			if url in opts.nonlocal:
+				logging.info(msg)
+			else:
+				raise RuntimeError(msg)
 
 		version = selection.getAttribute("version")
-		logging.debug("Adding dependency on version %s of %s" % (version, url))
 		req = feed.createElement("requires")
 		req.setAttribute("interface", url)
 		ver = feed.createElement("version")
 		ver.setAttribute("not-before", version)
 
-		#TODO: proper increment
-		ver.setAttribute("before", version + '-post')
+		next_version = Version.parse(version)
+		if opts.exact:
+			next_version = next_version.next()
+		else:
+			if opts.components > 0:
+				# ensure version has at least `c` components
+				components = itertools.chain(next_version.components, itertools.repeat(VersionComponent(0)))
+				components = list(itertools.islice(components, opts.components))
+				logging.debug("Freezing components: %s" % (list(map(str, components)),))
+				logging.debug("inc component %s -> %s" % (components[-1], components[-1].increment()))
+				components[-1] = components[-1].increment()
+				next_version = Version(components)
+			else:
+				# negative values we just pass through increment
+				next_version = next_version.increment(-opts.components)
+		logging.info("Adding dependency on v%s - v%s of %s" % (version, next_version, url))
+
+		ver.setAttribute("before", next_version.number)
 		req.appendChild(ver)
 
 		active_impl.appendChild(req)
 	
-	print(feed.toprettyxml())
+	if output_file is None:
+		filename = input_file.rstrip(r'\/')
+		filename = re.split(r'[\/]', filename)[-1]
+		stem, ext = os.path.splitext(filename)
+		output_file = "%s-freeze%s" % (stem, ext or '.xml')
+
+	output_contents = feed.toprettyxml()
+	output_contents = re.sub('<!-- Base64 Signature.*\Z', '', output_contents, flags=re.MULTILINE | re.DOTALL)
+	# cut out empty lines
+	output_contents = re.sub('^\s*\n', '', output_contents, flags=re.MULTILINE)
+	if output_file == '-':
+		print(output_contents, end='')
+	else:
+		with open(output_file, 'w') as out:
+			print(output_contents, file=out, end='')
+		logging.info("Wrote %s" % (output_file))
 
 if __name__ == '__main__':
 	main()
